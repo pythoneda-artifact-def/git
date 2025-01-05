e17bc91a08f86ba25becdc02cdce5f443a4ada51
Jose San Leandro Armendáriz <github@acm-sl.org>
2025-01-05 18:52:27 +0100

    Commit created with sync-pythoneda.sh

--- flake.nix
+++ flake.nix
@@ -39,7 +39,7 @@
         "pythoneda-shared-pythonlang-banner";
       inputs.pythoneda-shared-pythonlang-domain.follows =
         "pythoneda-shared-pythonlang-domain";
-      url = "github:pythoneda-shared-code-requests-def/jupyterlab/0.0.74";
+      url = "github:pythoneda-shared-code-requests-def/jupyterlab/0.0.75";
     };
     pythoneda-shared-artifact-code-events = {
       inputs.flake-utils.follows = "flake-utils";
@@ -48,7 +48,7 @@
         "pythoneda-shared-pythonlang-banner";
       inputs.pythoneda-shared-pythonlang-domain.follows =
         "pythoneda-shared-pythonlang-domain";
-      url = "github:pythoneda-shared-artifact-def/code-events/0.0.75";
+      url = "github:pythoneda-shared-artifact-def/code-events/0.0.76";
     };
     pythoneda-shared-code-requests-shared = {
       inputs.flake-utils.follows = "flake-utils";
@@ -57,7 +57,7 @@
         "pythoneda-shared-pythonlang-banner";
       inputs.pythoneda-shared-pythonlang-domain.follows =
         "pythoneda-shared-pythonlang-domain";
-      url = "github:pythoneda-shared-code-requests-def/shared/0.0.73";
+      url = "github:pythoneda-shared-code-requests-def/shared/0.0.74";
     };
     pythoneda-shared-pythonlang-banner = {
       inputs.flake-utils.follows = "flake-utils";
